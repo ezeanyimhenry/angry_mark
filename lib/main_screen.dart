2d97e7d6706232ac0a1840b0403ddae3f59b3025
Ezeanyim henry <ezeanyimhenry@gmail.com>
2024-08-06 11:03:47 +0100

    fix: adjust UI

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -2,8 +2,6 @@ import 'package:angry_mark/auth_screen.dart';
 import 'package:angry_mark/game_screen.dart';
 import 'package:angry_mark/instructions_screen.dart';
 import 'package:angry_mark/settings_screen.dart';
-// import 'package:angry_mark/instructions_screen.dart';
-// import 'package:angry_mark/settings_screen.dart';
 import 'package:flutter/material.dart';
 
 class MainMenu extends StatelessWidget {
@@ -24,163 +22,129 @@ class MainMenu extends StatelessWidget {
           // Overlay content
           Column(
             children: <Widget>[
-              // User icon at the top
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Stack(
-                  children: [
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: Container(
-                        width: 50,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          image: const DecorationImage(
-                            image: AssetImage(
-                                'assets/images/angry-mark.jpeg'), // Profile image
-                            fit: BoxFit.cover,
-                          ),
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(
-                            color: Colors.white, // Border color
-                            width: 2, // Border width
-                          ),
-                        ),
-                        // Add a tap gesture if needed
-                        child: Material(
-                          color: Colors.transparent,
-                          child: InkWell(
-                            borderRadius: BorderRadius.circular(
-                                25), // Circular border radius
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => const AuthScreen(),
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                      ),
-                    ),
-                    // Settings icon
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: Container(
-                        width: 50, // Width of the container
-                        height: 50, // Height of the container
-                        decoration: BoxDecoration(
-                          color: Colors.black
-                              .withOpacity(0.6), // Semi-transparent background
-                          borderRadius:
-                              BorderRadius.circular(8), // Rounded corners
-                        ),
-                        child: Center(
-                          child: IconButton(
-                            icon: const Icon(Icons.settings,
-                                size: 30, color: Colors.white),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => const SettingsScreen(),
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              //settings icon at the top
-
+              // Large Play Button
               Expanded(
                 child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      const SizedBox(height: 50),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          // Navigate to game screen
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const GameScreen()),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size(180, 50),
-                          backgroundColor: Colors.blue,
-                          foregroundColor: Colors.white,
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 15),
-                          textStyle: const TextStyle(fontSize: 18),
-                        ),
-                        icon: const Icon(Icons.play_arrow, size: 24),
-                        label: const Text(
-                          'Start Game',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          // Navigate to instructions screen
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    const InstructionsScreen()),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size(180, 50),
-                          backgroundColor: Colors.orange,
-                          foregroundColor: Colors.white,
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 15),
-                          textStyle: const TextStyle(fontSize: 18),
-                        ),
-                        icon: const Icon(Icons.help, size: 24),
-                        label: const Text(
-                          'Instructions',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          // Navigate to settings screen
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const SettingsScreen()),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size(180, 50),
-                          backgroundColor: Colors.grey,
-                          foregroundColor: Colors.white,
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 15),
-                          textStyle: const TextStyle(fontSize: 18),
-                        ),
-                        icon: const Icon(Icons.settings, size: 24),
-                        label: const Text(
-                          'Settings',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ],
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Navigate to game screen
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const GameScreen()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize:
+                          const Size(150, 150), // Adjust size as needed
+                      backgroundColor: Colors.white.withOpacity(0.8),
+                      foregroundColor: Colors.orange,
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(20),
+                      textStyle: const TextStyle(fontSize: 24),
+                    ),
+                    child: const Icon(Icons.play_arrow, size: 100),
                   ),
                 ),
               ),
             ],
+          ),
+          // Instructions Button
+          Positioned(
+            bottom: 20,
+            left: 20,
+            child: ElevatedButton.icon(
+              onPressed: () {
+                // Navigate to instructions screen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const InstructionsScreen()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(150, 50),
+                backgroundColor: Colors.orange,
+                foregroundColor: Colors.white,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                textStyle: const TextStyle(fontSize: 16),
+              ),
+              icon: const Icon(Icons.help, size: 24),
+              label: const Text(
+                'Instructions',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          // Profile image & settings icon
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Stack(
+              children: [
+                // Settings icon
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.6),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: IconButton(
+                        icon: const Icon(Icons.settings,
+                            size: 30, color: Colors.white),
+                        onPressed: () {
+                          // Uncomment and update if you have a settings screen
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const SettingsScreen(),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                // Profile image
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      image: const DecorationImage(
+                        image: AssetImage('assets/images/angry-mark.jpeg'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 2,
+                      ),
+                    ),
+                    child: Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(10),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const AuthScreen(),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
