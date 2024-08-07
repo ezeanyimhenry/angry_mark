914877a7409e9ace6473676893ca1723a29afd14
Jheezzy <horlajidey.olajide3@gmail.com>
2024-08-07 22:37:10 +0100

    adjust style

--- lib/forgot_password_screen.dart
+++ lib/forgot_password_screen.dart
@@ -18,37 +18,41 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
       backgroundColor: AppColors.primaryBackground,
       appBar: AppBar(
         backgroundColor: AppColors.primaryBackground,
+        foregroundColor: AppColors.textLigth,
         title: const Text(
           'Forgot Password',
-          style: TextStyle(color: AppColors.textLigth, fontSize: 25.0),
+          style: TextStyle(fontSize: 25.0),
         ),
       ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Enter your email to reset your password',
-                  style: TextStyle(color: AppColors.textLigth, fontSize: 25.0),
-                ),
-                const SizedBox(height: 20),
-                const EmailField(),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Processing Data')),
-                      );
-                    }
-                  },
-                  child: const Text('Reset Password'),
-                ),
-              ],
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Enter your email to reset your password',
+                    style:
+                        TextStyle(color: AppColors.textLigth, fontSize: 22.0),
+                  ),
+                  const SizedBox(height: 20),
+                  const EmailField(),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Processing Data')),
+                        );
+                      }
+                    },
+                    child: const Text('Reset Password'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
